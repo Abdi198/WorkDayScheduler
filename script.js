072066d7f99d2e555db36e38c1bc0c7d5be6d864
Abdi Ismail <sessiano@gmail.com>
2020-06-19 23:13:12 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// display current day on top of calendar 
+function todayDate() {
+    var d = new Date();
+    $("#currentDay").html(d.toDateString());
+    console.log(todayDate));
+};
+
+
+// display time-block for each bussiness hour of the day
+     
+
+
+// color code the time block according to current, past or future times 
+
+
+// user should be able to enter into the text time-blocks
+
+
+// user should be able to save text entered into local storage in browser
+    // need a function to store the saved data inside the browser window.localStorage.setItem
+
+// when user refreshes page entry should persist on the page
